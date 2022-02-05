481557b188937361d0f5ab5457dc9cc168cfbeee
Mohammed Abufarhana <realmohaa@mailbox.unideb.hu>
2022-02-05 17:15:48 +0100

    Implemented a User Model, Route, as well as Auth route with Registration feature

---
+++ routes/auth.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+
+// Registeration
+router.post("/register", async (req, res) => {
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_KEY).toString()
+    })
+    try{
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+    } catch(err){
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router
